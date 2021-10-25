5cb3550aee0d4f5b628e34ecf67d73632992206a
1002058460 <90251152+1002058460@users.noreply.github.com>
2021-10-25 08:38:08 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Cards from './Cards';
 import Main from './Main';
 import PersonList from './CardComponents';
+import Job from './Job';
 
 const rootElement = document.getElementById("root");
     ReactDOM.render(
       <BrowserRouter>
        <Switch>
         <Route exact path="/" component={Cards} />
+        <Route path="/Job" component={Job} />
         <Route path="/Main" component={Main} />
         <Route path="/CardComponents" component={PersonList} />
       </Switch>
